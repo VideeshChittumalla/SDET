eadb7aeee31204385cfe9e0e548cbe33a1951e4d
VideeshChittumalla <99386700+VideeshChittumalla@users.noreply.github.com>
2022-02-10 12:56:48 +0530

    Program for sorting of the array

---
+++ Assignment1ArraySort.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+  
+public class Assignment1ArraySort {
+  
+    public static void main(String[] args)
+    {
+        int[] arr = { 20,59,35,46,19,5,12,23 };
+        Arrays.sort(arr);
+        System.out.println("Sorted arr[] : %s", Arrays.toString(arr));
+    }
+}

ad4431eb6b762e21e8f7c878695b9d866577154e
VideeshChittumalla <99386700+VideeshChittumalla@users.noreply.github.com>
2022-02-12 13:08:12 +0530

    list of odd numbers b/w 79 to 187

---
+++ Assignment2FindOddNumbers.java
@@ -0,0 +1,12 @@
+public class Assignment2FindOddNumbers {
+
+	public static void main(String[] args) {
+		int number=187, i=79;
+		System.out.print("List of odd numbers between 79 to 187 are ");
+    while (i <= number) 
+    {
+			System.out.print(i + " ");
+      i = i + 2;
+		}
+	}
+}
